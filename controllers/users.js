4185f50f0be4c75c20131cc7e2768b69ba8aa587
ifotn <Rich.Freeman@GeorgianCollege.ca>
2025-03-11 14:34:35 -0400

    l9/4 - jwt httponly cookie on login

--- controllers/users.js
+++ controllers/users.js
@@ -1,8 +1,34 @@
 import express from 'express';
 import passport from 'passport';
 import User from '../models/user.js';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
+
+// private method to create jwt, called by login success
+const generateToken = (user) => {
+    // set up content to store user data in token
+    const payload = {
+        id: user._id,
+        username: user.username
+    };
+
+    const jwtOptions = {
+        expiresIn: '1hr'
+    };
+
+    // create token from above options
+    return jwt.sign(payload, process.env.PASSPORT_SECRET, jwtOptions);
+};
+
+// store jwt in HttpOnly cookie so client app can access but not modify it
+const setTokenCookie = (res, token) => {
+    res.cookie('authToken', token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'None'
+    });
+};
 
 // try to create new user account from username & password (passport hashes password)
 router.post('/register', async (req, res) => {
@@ -24,7 +50,14 @@ router.post('/login', async (req, res) => {
         const { user } = await User.authenticate()(req.body.username, req.body.password);
         if (user) {
             // user found
-            return res.json(200).json(user);
+            // create jwt containing user info
+            const authToken = generateToken(user);
+            
+            // store jwt in http-only cookie
+            setTokenCookie(res, authToken);
+
+            // return success response
+            return res.status(200).json({ token: authToken });
         }
         else {
             // user not found
@@ -34,7 +67,7 @@ router.post('/login', async (req, res) => {
     catch (err) {
         // something went wrong
         console.log(err);
-        return res.json(401).json(err);
+        return res.status(401).json(err);
     }
 });
 
