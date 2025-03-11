c22cdabe61ccfc2a9f01671f6bd52139df51ce48
ifotn <Rich.Freeman@GeorgianCollege.ca>
2025-03-11 12:48:37 -0400

    l9/1 - user model w/passport pckgs

---
+++ models/user.js
@@ -0,0 +1,25 @@
+import mongoose, { Mongoose } from "mongoose";
+
+// class used to set this model for user management
+import passportLocalMongoose from 'passport-local-mongoose';
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        minLength: 8,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 8
+    }
+});
+
+// inherit from passport-local-mongoose using the plugin() method to extend this model's functionality
+// this model is special - used by passport to manage user accounts; has methods like register & login
+userSchema.plugin(passportLocalMongoose);
+
+const User = mongoose.model('User', userSchema);
+export default User;
